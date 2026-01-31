31bcc543e102412f0bf41a3e5e1d3dea293426ef
t4ejxnn <tajuneom@gmail.com>
2026-01-31 13:51:51 +0900

    project basic setting

---
+++ src/main/java/com/DevLib/DevLibProjectApplication.java
@@ -0,0 +1,13 @@
+package com.DevLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevLibProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevLibProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DevLib/DevLibProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevLibProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
