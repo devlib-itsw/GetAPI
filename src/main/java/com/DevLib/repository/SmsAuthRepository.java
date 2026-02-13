3f18e9e6cd2a8fc14c0f76ce7cd1fa59bd86bae3
NohJeongWon <njwon19@gmail.com>
2026-02-13 11:22:34 +0900

    JWT 로테이션 적용 및 코드정리, 리팩토링

--- src/main/java/com/DevLib/repository/SmsAuthRepository.java
+++
@@ -1,7 +0,0 @@
-package com.DevLib.repository;
-
-import com.DevLib.domain.SmsAuth;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SmsAuthRepository extends CrudRepository<SmsAuth, String> {
-}
