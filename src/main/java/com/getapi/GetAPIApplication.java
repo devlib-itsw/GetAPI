6190c38af47ebbc4737faa7f9fbd5315378c76d3
njwon <njwon19@gmail.com>
2026-04-22 16:34:27 +0900

    append jparepo

--- src/main/java/com/getapi/GetAPIApplication.java
+++ src/main/java/com/getapi/GetAPIApplication.java
@@ -2,10 +2,12 @@ package com.getapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.getapi.user.repository")
 public class GetAPIApplication {
 
 	public static void main(String[] args) {
